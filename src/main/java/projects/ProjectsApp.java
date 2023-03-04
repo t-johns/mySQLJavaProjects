98330daa87722042ecba1987ec6ee17701b6951d
Trevor <trjohnson.1994@gmail.com>
2023-03-03 22:14:40 -0500

    Add week 9 CRUD assignment. Currently not creating successful connection.

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,12 +1,132 @@
 package projects;
 
-import projects.dao.DbConnection;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import projects.entity.Project;
+import projects.exception.DbException;
+import projects.service.ProjectService;
 
 public class ProjectsApp {
+  private Scanner scanner = new Scanner(System.in);
+  private ProjectService projectService = new ProjectService();
+
+  // @formatter:off
+  private List<String> operations = List.of(
+      "1) Add a project",
+      "2) Add a recipe"
+      );
+  // @formatter:on
 
   public static void main(String[] args) {
-    DbConnection.getConnection(); //connection test
+    new ProjectsApp().processUserSelections();
 
+  }
+
+
+  private void processUserSelections() {
+    boolean done = false;
+
+    while (!done) {
+
+      try {
+        int selection = getUserSelection();
+        
+        switch (selection) {
+          case -1:
+            done = exitMenu();
+            break;
+          case 1:
+            createProject();
+            break;
+            
+        default:
+          System.out.println("\n" + selection + " is not a valid selection. Try again.");
+        }
+
+      } catch (Exception e) {
+        System.out.println("\nError: " + e + " Try again.");
+      }
+    }
+
+  }
+
+  private void createProject() {
+    String projectName = getStringInput("Enter the project name");
+    BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours");
+    BigDecimal actualHours = getDecimalInput("Enter the actual hours");
+    Integer difficulty = getIntInput("Enter the project difficulty (1-5)");
+    String notes = getStringInput("Enter the project notes");
+    
+    Project project = new Project();
+    project.setProjectName(projectName);
+    project.setEstimatedHours(estimatedHours);
+    project.setActualHours(actualHours);
+    project.setDifficulty(difficulty);
+    project.setNotes(notes);
+    
+    Project dbProject = projectService.addProject(project); // add projectID from mySQL
+    
+    System.out.println("You have successfully created project: " + dbProject);
+    return;
+  }
+
+  private BigDecimal getDecimalInput(String prompt) {
+    String input = getStringInput(prompt);
+
+    if (Objects.isNull(input)) {
+      return null;
+    }
+    
+    try {
+      return new BigDecimal(input).setScale(2);
+    } catch (Exception e) {
+      throw new DbException(input + " is not a valid decimal number.");
+    }
+  }
+
+  private boolean exitMenu() {
+    System.out.println("Exiting the menu.");
+    return true;
+  }
+
+  private int getUserSelection() {
+    printOperations();
+
+    Integer input = getIntInput("Enter a menu selection");
+
+    return Objects.isNull(input) ? -1 : input;
+  }
+
+  private void printOperations() {
+    System.out.println(
+        "\nThese are the available selections. Press the Enter key to quit:");
+
+    operations.forEach(line -> System.out.println("   " + line));
+  }
+
+  private Integer getIntInput(String prompt) {
+    String input = getStringInput(prompt);
+
+    if (Objects.isNull(input)) {
+      return null;
+    }
+    
+    try {
+      return Integer.valueOf(input);
+    } catch (Exception e) {
+      throw new DbException(input + " is not a valid number. Try again.");
+    }
+
+  }
+
+  private String getStringInput(String prompt) {
+    System.out.print(prompt + ": ");
+    String input = scanner.nextLine();
+    
+    return input.isBlank() ? null : input.trim();
+    
   }
 
 }
